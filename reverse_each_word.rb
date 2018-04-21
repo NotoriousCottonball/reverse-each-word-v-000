a6d93d2055671f95ef38c319586a0349b69f853c
George  Pianka <gpianka1@gmail.com>
2018-04-21 14:35:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
   array = string.split
-  array.each {|word| word.reverse}
-  array.join(" ")
+  new_array = []
+  array.each {|word| new_array << word.reverse}
+  new_array.join(" ")
 end
